a477b741d1f3ebee2a061585b44dfed7261c3d73
devteam21 <hackmedev@gmail.com>
2021-10-09 10:49:13 +0700

    PEMECAHAN MASALAH 7

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -1,82 +1,71 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BallControl : MonoBehaviour
 {
-    // Rigidbody 2D bola
-    private Rigidbody2D rigidBody2D;
+    private Rigidbody2D rb2d;
+    public ScoreController scoreController;
 
-    // Titik asal lintasan bola saat ini
-    private Vector2 trajectoryOrigin;
-
-    // Besarnya gaya awal yang diberikan untuk mendorong bola
-    public float xInitialForce;
-    public float yInitialForce;
-    
-    void ResetBall()
+    private void Start()
     {
-        // Reset posisi menjadi (0,0)
-        transform.position = Vector2.zero;
-
-        // Reset kecepatan menjadi (0,0)
-        rigidBody2D.velocity = Vector2.zero;
-    }
-
-    void PushBall()
-    {
-
-        // Tentukan nilai acak antara 0 (inklusif) dan 2 (eksklusif)
-        float randomDirection = Random.Range(0, 2);
-
-        // Jika nilainya di bawah 1, bola bergerak ke kiri. 
-        // Jika tidak, bola bergerak ke kanan.
-        if (randomDirection < 1.0f)
-        {
-            // Gunakan gaya untuk menggerakkan bola ini.
-            rigidBody2D.AddForce(new Vector2(-xInitialForce, yInitialForce));
-        }
-        else
-        {
-            rigidBody2D.AddForce(new Vector2(xInitialForce, yInitialForce));
-        }
-
-    }
-
-    void RestartGame()
-    {
-        // Kembalikan bola ke posisi semula
+        rb2d = GetComponent<Rigidbody2D>();
         ResetBall();
 
-        // Setelah 2 detik, berikan gaya ke bola
-        Invoke("PushBall", 2);
+        //Problem 2 & 3
+        //Invoke("PushBall", 1); 
     }
 
-    void Start()
+    private void Update()
     {
-        trajectoryOrigin = transform.position;
+        //Problem 4 Start
+        /*if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 position = transform.position;
+            position.y += 0.5f;
+            transform.position = position;
+        } 
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Vector3 position = transform.position;
+            position.x -= 0.5f;
+            transform.position = position;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector3 position = transform.position;
+            position.x += 0.5f;
+            transform.position = position;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 position = transform.position;
+            position.y -= 0.5f;
+            transform.position = position;
+        }*/
+        //Problem 4 End
 
-        rigidBody2D = GetComponent<Rigidbody2D>();
-
-        // Mulai game
-        RestartGame();
+        //Problem 5 Start
+        Vector2 destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector3.MoveTowards(transform.position, destination, 5f * Time.deltaTime);
+        //Problem 5 End
     }
 
-    // Ketika bola beranjak dari sebuah tumbukan, rekam titik tumbukan tersebut
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        trajectoryOrigin = transform.position;
+        if (collision.gameObject.tag == "Box")
+        {
+            scoreController.IncreaseCurrentScore(1);
+            Destroy(collision.gameObject);
+        }
     }
 
-    // Untuk mengakses informasi titik asal lintasan
-    public Vector2 TrajectoryOrigin
+    private void ResetBall()
     {
-        get { return trajectoryOrigin; }
+        transform.position = Vector2.zero;
+        rb2d.velocity = Vector2.zero;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PushBall()
     {
-
+        rb2d.AddForce(new Vector2(10f, 8f));
     }
 }
